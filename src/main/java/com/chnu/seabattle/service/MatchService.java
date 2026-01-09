ab9c5f7f1b627f6adc2bdf98925cca702070c4d1
eugenius03 <82266188+eugenius03@users.noreply.github.com>
2026-01-10 00:07:39 +0200

    feat(gameservice): implement MatchService and MatchServiceImpl for match creation and joining
    Signed-off-by: eugenius03 <82266188+eugenius03@users.noreply.github.com>

---
+++ src/main/java/com/chnu/seabattle/service/MatchService.java
@@ -0,0 +1,22 @@
+package com.chnu.seabattle.service;
+
+import com.chnu.seabattle.entity.Match;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@Transactional
+public interface MatchService {
+
+//    UUID generateGuestId();
+
+    Match createMatch(UUID playerId);
+
+    Match joinMatch(UUID playerId, String inviteToken);
+
+//    Match getMatchById(Long matchId);
+
+    Match getMatchByInviteToken(String inviteToken);
+}
---
+++ src/main/java/com/chnu/seabattle/service/serviceImpl/MatchServiceImpl.java
@@ -0,0 +1,102 @@
+package com.chnu.seabattle.service.serviceImpl;
+
+import com.chnu.seabattle.entity.Match;
+import com.chnu.seabattle.entity.MatchPlayer;
+import com.chnu.seabattle.entity.MatchStatus;
+import com.chnu.seabattle.exception.GameRuleViolationException;
+import com.chnu.seabattle.repository.MatchPlayerRepository;
+import com.chnu.seabattle.repository.MatchRepository;
+import com.chnu.seabattle.service.MatchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.Random;
+import java.util.UUID;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MatchServiceImpl implements MatchService {
+
+    private final MatchRepository matchRepository;
+    private final MatchPlayerRepository matchPlayerRepository;
+
+    private MatchPlayer createMatchPlayer(Long matchId, UUID playerId) {
+        if (matchPlayerRepository.findByMatchIdAndUserId(matchId, playerId).isPresent()) {
+            throw new GameRuleViolationException("Player is already in the match");
+        }
+        MatchPlayer matchPlayer = new MatchPlayer();
+        matchPlayer.setUserId(playerId);
+        return matchPlayer;
+    }
+
+    private String generateInviteToken() {
+        return UUID.randomUUID().toString().replace("-", "").substring(0, 8);
+    }
+
+    @Override
+    public Match createMatch(UUID playerId) {
+        Match match = new Match();
+        match.setStatus(MatchStatus.WAITING);
+        match.setInviteToken(generateInviteToken());
+
+        match = matchRepository.save(match);
+
+        MatchPlayer matchPlayer = createMatchPlayer(match.getId(), playerId);
+        matchPlayer.setMatch(match);
+        matchPlayer.setLastSeenAt(Instant.now());
+        matchPlayer.setReconnectToken(generateInviteToken());
+        matchPlayer.setConnected(true);
+
+        match.getPlayers().add(matchPlayer);
+        matchRepository.save(match);
+
+        return match;
+    }
+
+    @Override
+    public Match joinMatch(UUID playerId, String inviteToken) {
+        Match match = matchRepository.findByInviteToken(inviteToken)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid invite token"));
+
+        if (match.getStatus() != MatchStatus.WAITING) {
+            throw new GameRuleViolationException("Match is not joinable");
+        }
+
+        if (match.getPlayers().size() >= 2) {
+            throw new GameRuleViolationException("Match is already full");
+        }
+
+        MatchPlayer matchPlayer = createMatchPlayer(match.getId(), playerId);
+        matchPlayer.setReconnectToken(generateInviteToken());
+        matchPlayer.setMatch(match);
+        matchPlayer.setLastSeenAt(Instant.now());
+        matchPlayer.setConnected(true);
+
+        match.getPlayers().add(matchPlayer);
+
+        Random random = new Random();
+
+        MatchPlayer firstPlayer = match.getPlayers().get(random.nextInt(match.getPlayers().size()));
+        match.setCurrentPlayerTurnId(firstPlayer.getUserId());
+        match.setStatus(MatchStatus.PLANNING);
+        matchRepository.save(match);
+
+        return match;
+
+    }
+
+//    @Override
+//    public Match getMatchById(Long matchId) {
+//        return matchRepository.findById(matchId)
+//                .orElseThrow(() -> new IllegalArgumentException("Match not found"));
+//    }
+
+    @Override
+    public Match getMatchByInviteToken(String inviteToken) {
+        return matchRepository.findByInviteToken(inviteToken)
+                .orElseThrow(() -> new IllegalArgumentException("Match not found"));
+    }
+}
