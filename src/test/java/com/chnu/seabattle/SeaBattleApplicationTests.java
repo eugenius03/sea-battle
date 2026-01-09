3f82050129dc901344069ff7bf5bc9c8e85b0a91
eugenius03 <82266188+eugenius03@users.noreply.github.com>
2026-01-10 00:07:53 +0200

    refactor(tests): disable SeaBattleApplicationTests class and context load test
    Signed-off-by: eugenius03 <82266188+eugenius03@users.noreply.github.com>

--- src/test/java/com/chnu/seabattle/SeaBattleApplicationTests.java
+++ src/test/java/com/chnu/seabattle/SeaBattleApplicationTests.java
@@ -1,13 +1,10 @@
 package com.chnu.seabattle;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SeaBattleApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class SeaBattleApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
